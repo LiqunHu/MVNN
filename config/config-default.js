decb31967d0c39ac3b481357c07a6516ee6486d5
huliqun <huliquns@126.com>
2017-04-27 17:20:53 +0800

    fix

--- config/config-default.js
+++ config/config-default.js
@@ -6,7 +6,7 @@ var config = {
         username: 'root',
         password: '123456',
         host: 'localhost',
-        port: 33306
+        port: 3306
     },
     redisCache: true,
     // for redis
