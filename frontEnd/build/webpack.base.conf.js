11b8d78ef734c23fed140d867eefd28f0b004f4c
huliqun <huliquns@126.com>
2017-04-28 10:38:10 +0800

    fix docker

--- frontEnd/build/webpack.base.conf.js
+++ frontEnd/build/webpack.base.conf.js
@@ -19,7 +19,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.js',
       'vue-router$': 'vue-router/dist/vue-router.common.js',
-      src: path.resolve(__dirname, '../src')
+      src: path.resolve(__dirname, '../src'),
+      commonFunc: path.resolve(__dirname, '../src/assets/js/common.js')
     },
     extensions: ['.js', '.vue', '.less']
   },
