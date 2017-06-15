03daf51c6b59cee97788a58de71b0bd464f90ef6
huliqun <huliquns@126.com>
2017-06-15 12:05:27 +0800

    commit

--- frontEnd/start.sh
+++ frontEnd/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -p 127.0.0.1:8000:8000 --name ncaweb --link ncaserver:ncaserver -v $PWD:/webapp -w /webapp -it node:7.9.0 npm run dev
+docker run --rm -p 127.0.0.1:8000:8000 --name mvnnweb --link mvnnserver:mvnnserver -v $PWD:/webapp -w /webapp -it node:8.1 npm run dev
